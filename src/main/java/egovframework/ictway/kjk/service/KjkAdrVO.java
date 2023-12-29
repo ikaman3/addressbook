643efcd05f94e74dacfcd5024a33b23565300e44
kjk <jinkwang930@ictway.co.kr>
2023-12-29 11:35:47 +0900

    Feat: KjkadrVO 생성

---
+++ src/main/java/egovframework/ictway/kjk/service/KjkAdrVO.java
@@ -0,0 +1,54 @@
+package egovframework.ictway.kjk.service;
+
+import java.io.Serializable;
+
+import egovframework.com.cmm.ComDefaultVO;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+* 주소록 정보 처리를 위한 VO 클래스
+* @author ICTWAY
+* @since 2023.12.29
+* @version 1.0
+* @Modification
+* <pre>
+*2023.12.29 / 김진광 / 최초생성
+* </pre>
+* @see
+*/
+@NoArgsConstructor
+@Getter @Setter
+public class KjkAdrVO extends ComDefaultVO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	/** 주소록아이디 */
+	private long adrId;
+
+	/** 주소록제목 */
+	private String adrSj;
+
+	/** 주소록내용 */
+	private String adrCn;
+
+	/** 사용여부 */
+	private String useAt;
+
+	/** 첨부파일아이디 */
+	private String atchFileId;
+
+	/** 최초등록일시 */
+	private String frstRegistPnttm;
+
+	/** 최초등록아이디 */
+	private String frstRegisterId;
+
+	/** 최종수정일시 */
+	private String lastUpdtPnttm;
+
+	/** 최종수정아이디 */
+	private String lastUpdusrId;
+
+}
