22a7c542483cb90bf1aa8db18fa7aa25f399b94a
yja <jiae8596@ictway.co.kr>
2024-01-04 13:54:45 +0900

    Feat: 패키지 생성 및 내용 수정
    yja 패키지를 생성하였고 adr을 adrITA로 변경
    
    Resolve: #4

---
+++ src/main/java/egovframework/ictway/yja/impl/AdrITADAO.java
@@ -0,0 +1,77 @@
+package egovframework.ictway.yja.impl;
+
+import java.util.List;
+
+import org.egovframe.rte.psl.dataaccess.EgovAbstractMapper;
+import org.springframework.stereotype.Repository;
+
+import egovframework.ictway.yja.service.AdrITAVO;
+
+/**
+ * 주소록 정보 관리를 위한 데이터 처리 클래스
+ * @author ICTWAY
+ * @since 2024.12.29
+ * @version 1.0
+ * @Modification
+ * <pre>
+ *2024.12.29 / 유지애 / 최초생성
+ * </pre>
+ * @see
+ */
+@Repository("adrITADAO")
+public class AdrITADAO extends EgovAbstractMapper {
+	
+	/**
+	 * 검색된 목록을 조회한다.
+	 * @param adrITAVO - 조회할 정보가 담긴 VO
+	 * @return 목록
+	 */
+	public List<?> selectAdrITAList(AdrITAVO adrITAVO) {
+		return selectList("AdrITADAO.selectAdrITAList", adrITAVO);
+	}
+
+	/**
+	 * 검색된 목록의 총 건 수를 조회한다.
+	 * @param adrITAVO - 조회할 정보가 담긴 VO
+	 * @return 목록 건 수
+	 */
+	public int selectAdrITAListCnt(AdrITAVO adrITAVO) {
+		return (Integer)selectOne("AdrITADAO.selectAdrITAListCnt", adrITAVO);
+	}
+	
+	/**
+	 * 정보를 조회한다.
+	 * @param adrITAVO - 조회할 정보가 담긴 VO
+	 * @return 목록
+	 */
+	public AdrITAVO selectAdrITADetail(AdrITAVO adrITAVO) {
+		return selectOne("AdrITADAO.selectAdrITADetail", adrITAVO);
+	}
+	
+	/**
+	 * 정보를 등록한다.
+	 * @param adrITAVO - 등록할 정보가 담긴 VO
+	 * @return void
+	 */
+	public void insertAdrITAAct(AdrITAVO adrITAVO) {
+		insert("AdrITADAO.insertAdrITAAct", adrITAVO);
+	}
+
+	/**
+	 * 정보를 수정한다.
+	 * @param adrITAVO - 수정할 정보가 담긴 VO
+	 * @return void
+	 */
+	public void updateAdrITAAct(AdrITAVO adrITAVO) {
+		update("AdrITADAO.updateAdrITAAct", adrITAVO);
+	}
+
+	/**
+	 * 정보를 삭제한다.
+	 * @param adrITAVO - 삭제할 정보가 담긴 VO
+	 * @return void
+	 */
+	public void deleteAdrITAAct(AdrITAVO adrITAVO) {
+		update("AdrITADAO.deleteAdrITAAct", adrITAVO);
+	}
+}
---
+++ src/main/java/egovframework/ictway/yja/service/AdrITAVO.java
@@ -0,0 +1,89 @@
+package egovframework.ictway.yja.service;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+* 주소정보 정보 처리를 위한 VO 클래스
+* @author ICTWAY
+* @since 2024.01.04
+* @version 1.0
+* @Modification
+* <pre>
+*2024.01.04 / 유지애 / 최초생성
+* </pre>
+* @see
+*/
+@NoArgsConstructor
+@Getter @Setter
+public class AdrITAVO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	/** 주소록일련번호 */
+	private long adbkSn;
+
+	/** 사용자명칭 */
+	private String userNm;
+
+	/** 생년월일 */
+	private String brthdy;
+
+	/** 성별코드 */
+	private String sexdstnCode;
+
+	/** 주소 */
+	private String adres;
+
+	/** 상세주소 */
+	private String detailAdres;
+
+	/** 휴대폰번호 */
+	private String moblphonNo;
+
+	/** 이메일주소 */
+	private String emailaddr;
+
+	/** 메모 */
+	private String memo;
+
+	/** 그룹명칭 */
+	private String groupNm;
+
+	/** 회사명칭 */
+	private String cmpnyNm;
+
+	/** 부서명칭 */
+	private String deptNm;
+
+	/** 직급명칭 */
+	private String clsfNm;
+
+	/** 즐겨찾기여부 */
+	private String bkmkAt;
+
+	/** 사진명칭 */
+	private String photoNm;
+
+	/** 사진경로 */
+	private String photoCours;
+
+	/** 사진확장자명칭 */
+	private String photoExtsnNm;
+
+	/** 등록일시 */
+	private String registDt;
+
+	/** 수정일시 */
+	private String updtDt;
+
+	/** 등록사용자명칭 */
+	private String registUserNm;
+
+	/** 삭제여부 */
+	private String deleteAt;
+
+}
