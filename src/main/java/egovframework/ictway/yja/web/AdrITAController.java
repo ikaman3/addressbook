c85a770b6e8d131aaa1daa685994aba5001757a8
yja <jiae8596@ictway.co.kr>
2024-01-08 14:34:17 +0900

    Feat: 상세페이지 사진 조회 기능 구현
    사진 조회 기능 구현
    
    Resolve: #83

--- src/main/java/egovframework/ictway/yja/web/AdrITAController.java
+++ src/main/java/egovframework/ictway/yja/web/AdrITAController.java
@@ -1,9 +1,18 @@
 package egovframework.ictway.yja.web;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.egovframe.rte.fdl.property.EgovPropertyService;
 import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
@@ -11,9 +20,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.SessionVO;
+import egovframework.com.cmm.service.EgovFileMngService;
+import egovframework.com.cmm.service.EgovFileMngUtil;
+import egovframework.com.cmm.service.FileVO;
+import egovframework.com.cmm.web.EgovImageProcessController;
 import egovframework.ictway.yja.service.AdrITAService;
 import egovframework.ictway.yja.service.AdrITAVO;
 import lombok.extern.slf4j.Slf4j;
@@ -41,6 +58,18 @@ public class AdrITAController {
 
 	@Resource(name = "egovMessageSource")
 	EgovMessageSource egovMessageSource;
+	
+	@Resource(name = "EgovFileMngService")
+	private EgovFileMngService fileMngService;
+
+	@Resource(name = "EgovFileMngUtil")
+	private EgovFileMngUtil fileUtil;
+	
+	
+	@Resource(name = "EgovFileMngService")
+	private EgovFileMngService fileService;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(EgovImageProcessController.class);
 	
 	/**
 	 * 주소록 목록조회
@@ -116,9 +145,26 @@ public class AdrITAController {
      * @throws Exception
      */
     @RequestMapping("/ictway/yja/registAdrITAAct.do")
-    public ModelAndView registAdrITAAct(AdrITAVO adrITAVO, ModelMap model) throws Exception { 
+    public ModelAndView registAdrITAAct(MultipartHttpServletRequest multiRequest, AdrITAVO adrITAVO, ModelMap model) throws Exception { 
 
     	ModelAndView mav = new ModelAndView("jsonView");
+    	
+    	List<FileVO> result = null;
+    	String atchFileId = "";
+    	String extension = "";
+    	String adrs = "";
+
+		final Map<String, MultipartFile> files = multiRequest.getFileMap();
+		if (!files.isEmpty()) {
+			result = fileUtil.parseFileInf(files, "ITA_", 0, "", "");
+			atchFileId = result.get(0).getStreFileNm();
+			extension = result.get(0).fileExtsn;
+			adrs = result.get(0).getFileStreCours();
+		}
+    	
+    	adrITAVO.setPhotoNm(atchFileId);
+    	adrITAVO.setPhotoExtsnNm(extension);
+    	adrITAVO.setPhotoCours(adrs);
     	
 		String adrId = adrITAService.registAdrITAAct(adrITAVO);
 		mav.addObject("adrId", adrId);
@@ -172,4 +218,74 @@ public class AdrITAController {
 		
 		return mav;
     }
+    
+    @RequestMapping("/ictway/yja/getImage.do")
+	public void getImageInf(AdrITAVO adrITAVO, SessionVO sessionVO, ModelMap model, @RequestParam Map<String, Object> commandMap, HttpServletResponse response) throws Exception {
+
+    	AdrITAVO fvo = adrITAService.selectAdrITADetail(adrITAVO);
+		
+    	String fileStreCours = fvo.getPhotoCours();
+    	String streFileNm = fvo.getPhotoNm();
+    	
+		File file = new File(fileStreCours, streFileNm);
+		FileInputStream fis = null;
+//		new FileInputStream(file);
+
+		BufferedInputStream in = null;
+		ByteArrayOutputStream bStream = null;
+		try {
+			fis = new FileInputStream(file);
+			in = new BufferedInputStream(fis);
+			bStream = new ByteArrayOutputStream();
+			int imgByte;
+			while ((imgByte = in.read()) != -1) {
+				bStream.write(imgByte);
+			}
+
+			String type = "";
+
+			if (fvo.getPhotoExtsnNm() != null && !"".equals(fvo.getPhotoExtsnNm())) {
+				if ("jpg".equals(fvo.getPhotoExtsnNm().toLowerCase())) {
+					type = "image/jpeg";
+				} else {
+					type = "image/" + fvo.getPhotoExtsnNm().toLowerCase();
+				}
+				type = "image/" + fvo.getPhotoExtsnNm().toLowerCase();
+
+			} else {
+				LOGGER.debug("Image fileType is null.");
+			}
+
+			response.setHeader("Content-Type", type);
+			response.setContentLength(bStream.size());
+			bStream.writeTo(response.getOutputStream());
+			response.getOutputStream().flush();
+			response.getOutputStream().close();
+
+		} catch (IOException e) {
+			LOGGER.debug("{}", e);
+		} finally {
+			if (bStream != null) {
+				try {
+					bStream.close();
+				} catch (IOException est) {
+					LOGGER.debug("IGNORED: {}", est.getMessage());
+				}
+			}
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException ei) {
+					LOGGER.debug("IGNORED: {}", ei.getMessage());
+				}
+			}
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (IOException efis) {
+					LOGGER.debug("IGNORED: {}", efis.getMessage());
+				}
+			}
+		}
+	}
 }
